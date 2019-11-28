f34d38390f94914a37999d343930c0237649e197
shafie <shafiemukhre@gmail.com>
2019-11-28 15:27:28 -0800

    update

---
+++ static/js/main.js
@@ -0,0 +1,54 @@
+$(window).load(function() {
+    $("#preloader-wrapper").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+
+     
+    //animated header class
+    $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 200) {
+        //console.log('a');
+        $(".navigation").addClass("animated");
+    } else {
+        //console.log('a');
+        $(".navigation").removeClass("animated");
+    }});
+
+
+
+    $(".gallery-slider").owlCarousel(
+        {
+        pagination : true,
+        autoPlay : 5000,
+        itemsDesktop  :  [1500,4],
+        itemsDesktopSmall :  [979,3]
+        }
+    );
+
+    $(".technology-slider").owlCarousel(
+        {
+        pagination : true,
+        autoPlay : 5000,
+        itemsDesktop  :  [1500,2],
+        itemsDesktopSmall :  [979,1]
+        }
+    );
+
+
+    // Gallery Popup
+    $('.image-popup').magnificPopup({type:'image'});
+
+
+
+});
+
+
+
+
+
+
+
